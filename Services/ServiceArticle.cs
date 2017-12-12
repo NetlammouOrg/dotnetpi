6c72fb21e968a847f35f20b8edb0086c16cbd874
WiemJebalia <33383201+WiemJebalia@users.noreply.github.com>
2017-12-12 01:55:03 +0100

    Add files via upload

---
+++ Services/ServiceArticle.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity;
+using Data.Infrastructure;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ServiceArticle : Service<article>, IServiceArticle
+    {
+        private static IDatabaseFactory dbf = new DatabaseFactory();
+        private static IUnitOfWork ut = new UnitOfWork(dbf);
+        public ServiceArticle() : base(ut)
+        {
+
+        }
+    }
+}
